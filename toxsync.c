460e19228de7e6afc2dafb71eec012fc6917ea6c
Mark Winter <mark@tox.im>
2014-10-19 16:22:05 +0100

    Some refactoring

--- toxsync.c
+++ toxsync.c
@@ -21,9 +21,10 @@ void print_help() {
 	print_title();
 	mvprintw(2, 0, "%s", "Commands");
 	mvprintw(3, 3, "%s", "/help - prints this message");
-	mvprintw(4, 3, "%s", "/add <tox id> - adds tox id to sycn list");
-	mvprintw(5, 3, "%s", "/sync - syncs files");
-	mvprintw(6, 3, "%s", "/quit - exits toxsync");
+	mvprintw(4, 3, "%s", "/id - print your tox id");
+	mvprintw(5, 3, "%s", "/add <tox id> - adds tox id to sycn list");
+	mvprintw(6, 3, "%s", "/sync - syncs files");
+	mvprintw(7, 3, "%s", "/quit - exits toxsync");
 	refresh();
 }
 
@@ -78,7 +79,7 @@ void _callback_file_send_request(Tox* tox, int friednumber, uint8_t filenumber, 
 						const uint8_t* filename, uint16_t filename_length, void* userdata) {
 }
 
-uint8_t hex_string_to_bin(char* string) {
+uint8_t* hex_string_to_bin(char* string) {
 	size_t i, len = strlen(string) / 2;
 	uint8_t* ret = malloc(len);
 	char* pos = string;
@@ -89,18 +90,21 @@ uint8_t hex_string_to_bin(char* string) {
 	return ret;
 }
 
-int main() {
-	initscr();
-	getmaxyx(stdscr, ROW, COL);
+/* Allocates new tox structure and bootstraps 
+ * Returns 0 on success
+ * Returns -1 on failure
+ */
+int init_tox() {
+	Tox_Options options;
+	options.ipv6enabled = 1;
+	options.udp_disabled = 0;
+	options.proxy_enabled = 0;
 
-	/* Set up Tox stuff */
-	Tox_Options* options = malloc(sizeof(Tox_Options));
-	options->ipv6enabled = 1;
-	options->udp_disabled = 0;
-	options->proxy_enabled = 0;	
+	tox = tox_new(&options);
 
-	tox = tox_new(options);
-	free(options);
+	/* Failed creating new tox */
+	if (!tox)
+		return -1;
 
 	load_data();
 
@@ -109,18 +113,40 @@ int main() {
 	tox_callback_file_control(tox, _callback_file_control, NULL);
 	tox_callback_file_send_request(tox, _callback_file_send_request, NULL);
 
+	char* address = "37.59.102.176";
+	uint16_t port = 33445;
+	unsigned char* binary_string = hex_string_to_bin("B98A2CEAA6C6A2FADC2C3632D284318B60FE5375CCB41EFA081AB67F500C1BOB");
+	int res = tox_bootstrap_from_address(tox, address, port, binary_string);
+	free(binary_string);
+
+	/* Failed bootstrapping */
+	if (res != 1)
+		return -1;
+
+	return 0;
+}
+
+void shutdown() {
+	getch();
+	save_data();
+	tox_kill(tox);
+	endwin();
+}
+
+int main() {
+	initscr();
+	getmaxyx(stdscr, ROW, COL);
+
+	int res = init_tox();
+
+	if (res == -1)
+		return -1;
+
 	print_help();
 	print_bottom_bar();
 	refresh();
 
-	char* address = "37.59.102.176";
-	uint16_t port = 33445;
-	unsigned char* binary_string = hex_string_to_bin("B98A2CEAA6C6A2FADC2C3632D284318B60FE5375CCB41EFA081AB67F500C1B0B");
-	int res = tox_bootstrap_from_address(tox, address, port, binary_string);	
+	shutdown();
 
-	getch();
-	save_data();		
-	tox_kill(tox);
-	endwin();
 	return 0;
 }
